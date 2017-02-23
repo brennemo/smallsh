a6751ab92e694bdf722423b4733ed4e89b4cf369
Morgan <brennemo@oregonstate.edu>
2017-02-23 14:19:02 -0500

    get line from user and tokenize

---
+++ smallsh/smallsh.c
@@ -0,0 +1,28 @@
+#include <sys/types.h>		//pid_t, etc. 
+//#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_CHAR 2048
+#define MAX_ARGS 512
+
+void main() {
+	char commandLine[MAX_CHAR];
+	char *ptr; 
+
+	while (1) {
+		printf(": ");		//use ': ' as prompt for each command line 
+		fflush(stdout);	//flush output buffer immediately after each output 
+
+		fgets(commandLine, MAX_CHAR, stdin);
+
+		ptr = strtok(commandLine, " ");
+
+		while (ptr != NULL) {
+			printf("%s\n", ptr);
+			ptr = strtok(NULL, " ");
+		} 
+	}
+
+
+}
