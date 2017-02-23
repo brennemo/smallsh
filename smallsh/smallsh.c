330539719b9c8622d9db2db65b22d8622fb00427
Morgan <brennemo@oregonstate.edu>
2017-02-23 14:30:44 -0500

    get number of "arguments"

--- smallsh/smallsh.c
+++ smallsh/smallsh.c
@@ -9,20 +9,21 @@
 void main() {
 	char commandLine[MAX_CHAR];
 	char *ptr; 
+	int numArgs = 0;
 
 	while (1) {
 		printf(": ");		//use ': ' as prompt for each command line 
 		fflush(stdout);	//flush output buffer immediately after each output 
 
 		fgets(commandLine, MAX_CHAR, stdin);
-
+		
 		ptr = strtok(commandLine, " ");
-
 		while (ptr != NULL) {
 			printf("%s\n", ptr);
 			ptr = strtok(NULL, " ");
+			++numArgs;
 		} 
+		printf("%d\n", numArgs);
 	}
-
 
 }
