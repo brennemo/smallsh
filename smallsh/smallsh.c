fd0c07b18dec2f5b1bf03c9ca36dfb3b11201e50
Morgan <brennemo@oregonstate.edu>
2017-02-24 10:39:35 -0500

    get built-in args

--- smallsh/smallsh.c
+++ smallsh/smallsh.c
@@ -12,12 +12,13 @@ void smallsh_status();
 
 int main() {
 	char commandLine[MAX_CHAR];
-	char *ptr; 
-	int i, numArgs;			 
+	char *ptr; //*inputFile, *outputFile; 
+	int i, numArgs; //inputIndex, outputIndex;
 	char *args[MAX_ARGS];
 
 	while (1) {
 		numArgs = 0; 
+		//inputIndex = outputIndex = -1;
 
 		printf(": ");		//use ': ' as prompt for each command line 
 		fflush(stdout);	//flush output buffer immediately after each output 
@@ -28,17 +29,23 @@ int main() {
 		printf("command: %s\n", ptr);
 		
 		while (ptr != NULL) {
+			/*
 			if (strcmp(ptr, ">") == 0) {
 				printf("input!\n");
+				inputIndex = numArgs;
 			}   
 			
 			else if (strcmp(ptr, "<") == 0) {
 				printf("output!\n");
+				outputIndex = numArgs; 
 			}
+			*/
+			/*
 			else if (strcmp(ptr, "&") == 0) {
 				printf("background process!\n");
-			}
-			args[numArgs] = ptr;		//store 
+			}  
+			*/
+			args[numArgs] = ptr;		//store command/argument in array 
 			ptr = strtok(NULL, " \n");
 			numArgs++;
 		} 
@@ -49,6 +56,41 @@ int main() {
 			printf("%s ", args[i]);
 		}
 		printf("\n");
+
+		//Check for background process at end of command 
+		if (strcmp(args[numArgs - 1], "&") == 0) {
+			printf("background process!\n");
+		}
+			
+		//Check for built-in commands
+		if (strcmp(args[0], "exit") == 0 || strcmp(args[0], "cd") == 0 || strcmp(args[numArgs - 1], "&") == 0) {
+			if (strcmp(args[0], "exit") == 0) {
+				smallsh_exit();
+			}
+			else if (strcmp(args[0], "cd") == 0) {
+				smallsh_cd();
+			}
+			else if (strcmp(args[0], "status") == 0) {
+				smallsh_status();
+			}
+
+			//Check for input and output files	and store names
+			/*
+			if (inputIndex != -1 && inputIndex < numArgs - 1) {	//there is a '>' within bounds 
+				inputFile = args[inputIndex + 1];
+				printf("Input file: %s\n", inputFile);
+			}
+			if (outputIndex != -1 && outputIndex < numArgs - 1) {	//there is a '>' within bounds 
+				outputFile = args[outputIndex + 1];
+				printf("Output file: %s\n", outputFile);
+			}
+			*/
+
+		}
+		//Run other commands with fork(), exec(), and waitpid()
+		else {
+			printf("Other shell commands!\n");
+		}
 	}
 
 	return 0;
@@ -56,13 +98,13 @@ int main() {
 }		
 
 void smallsh_exit() {
-
+	printf("EXIT!\n");
 }
 
 void smallsh_cd() {
-
+	printf("CD!\n");
 }
 
 void smallsh_status() {
-
+	printf("STATUS!\n");
 }
